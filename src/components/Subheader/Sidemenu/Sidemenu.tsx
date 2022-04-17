22c1fead4d899d077bfb187c2d0e92a2263c3969
Aaron Lin <aaronlin74@gmail.com>
2022-04-17 22:07:33 +1000

    feat: add sidemenu that clones amazon site
    - Sidemenu: for demonstration purpose, only add a few dummy links to clone amazon sidemenu. Sign in functionality has not yet been implemented.

---
+++ src/components/Subheader/Sidemenu/Sidemenu.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineClose } from 'react-icons/ai';
+import { CgProfile } from 'react-icons/cg';
+import './Sidemenu.scss';
+
+interface Props {
+  isSidemenuOpen: boolean;
+  closeSidemenu: () => void;
+}
+function Sidemenu({ isSidemenuOpen, closeSidemenu }: Props) {
+  return (
+    <div
+      className={` ${
+        isSidemenuOpen ? 'sidemenu sidemenu--visible' : 'sidemenu'
+      }`}
+    >
+      <div className='sidemenu__background'>
+        <AiOutlineClose
+          className='sidemenu__closeIcon'
+          onClick={closeSidemenu}
+          tabIndex={0}
+          aria-label='close sidemenu'
+          title='close sidemenu'
+        />
+      </div>
+      <div className='sidemenu__canvas'>
+        <Link
+          to='/'
+          className='sidemenu__customerProfile'
+          onClick={closeSidemenu}
+        >
+          <CgProfile />
+          Hello, Sign in
+        </Link>
+        <div className='sidemenu__content'>
+          <ul className='sidemenu__list'>
+            <li>
+              <span className='sidemenu__item sidemenu__title'>Trending</span>
+            </li>
+            <li>
+              <Link to='/' className='sidemenu__item' onClick={closeSidemenu}>
+                Best Sellers
+              </Link>
+            </li>
+            <li>
+              <Link to='/' className='sidemenu__item' onClick={closeSidemenu}>
+                New Releases
+              </Link>
+            </li>
+            <li>
+              <Link to='/' className='sidemenu__item' onClick={closeSidemenu}>
+                Movers and Shakers
+              </Link>
+            </li>
+            <li className='sidemenu__separator' />
+            <li>
+              <span className='sidemenu__item sidemenu__title'>
+                Digital Content And Devices
+              </span>
+            </li>
+            <li>
+              <Link to='/' className='sidemenu__item' onClick={closeSidemenu}>
+                Echo & Alexa
+              </Link>
+            </li>
+            <li>
+              <Link to='/' className='sidemenu__item' onClick={closeSidemenu}>
+                Kindle E-readers & Books
+              </Link>
+            </li>
+            <li>
+              <Link to='/' className='sidemenu__item' onClick={closeSidemenu}>
+                Amazon Fire TV
+              </Link>
+            </li>
+            <li>
+              <Link to='/' className='sidemenu__item' onClick={closeSidemenu}>
+                Amazon Prime Video
+              </Link>
+            </li>
+            <li>
+              <Link to='/' className='sidemenu__item' onClick={closeSidemenu}>
+                Amazon Music
+              </Link>
+            </li>
+            <li>
+              <Link to='/' className='sidemenu__item' onClick={closeSidemenu}>
+                Audible Audiobooks
+              </Link>
+            </li>
+            <li className='sidemenu__separator' />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidemenu;
