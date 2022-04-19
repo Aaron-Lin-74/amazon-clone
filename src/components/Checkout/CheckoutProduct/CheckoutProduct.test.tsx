9e45e8d6368c1abe20b93849b1147345c2f5b6e9
Aaron Lin <aaronlin74@gmail.com>
2022-04-19 23:52:15 +1000

    feat: add CheckoutProduct component
    - CheckoutProduct: represents the individual shopping cart item on checkout page. We mimic the amazon site, so we can change quantity and delete from cart. The save for later and stock functionality have not been implemented.

---
+++ src/components/Checkout/CheckoutProduct/CheckoutProduct.test.tsx
@@ -0,0 +1,30 @@
+import { screen, render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import CheckoutProduct from './CheckoutProduct';
+
+describe('CheckoutProduct component', () => {
+  const mockProduct = {
+    id: '1',
+    image: 'http://mockimge',
+    title: 'mock title',
+    price: 100,
+    quantity: 1,
+    stock: 'In stock',
+    freeShipping: true,
+  };
+  test('should render the component', () => {
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    render(<CheckoutProduct {...mockProduct} />, { wrapper: BrowserRouter });
+    screen.getByRole('img', {
+      name: /mock title/i,
+    });
+    screen.getByText('mock title');
+    screen.getByText('In stock');
+    screen.getByText('Eligible for FREE Shipping');
+    screen.getByText('This will be a gift');
+    expect(
+      (screen.getByRole('option', { name: '1' }) as HTMLOptionElement).selected
+    ).toBe(true);
+    screen.getByText('$100.00');
+  });
+});
