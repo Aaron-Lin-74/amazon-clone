d02664e26d91425fc8d0e5e571d718d949beced9
Aaron Lin <aaronlin74@gmail.com>
2022-04-24 23:27:04 +1000

    feat: add magnifier functionality for Gallery
    - Gallery: implement magnifier functionality for the gallery images. Modify the style so that the image will fit the size allocated when resizes the window.

--- src/pages/Product/Gallery/Gallery.tsx
+++ src/pages/Product/Gallery/Gallery.tsx
@@ -5,13 +5,22 @@ type Props = {
   images: { thumbnail: string; image: string }[];
 };
 function Gallery({ images }: Props) {
+  const magnifierHeight: number = 500;
+  const magnifierWidth: number = 500;
+  const zoomLevel: number = 2;
+
   const [imageIndex, setImageIndex] = useState<number>(0);
+  const [showMagnifier, setShowMagnifier] = useState<boolean>(false);
+  const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
+  const [[x, y], setXY] = useState<[number, number]>([0, 0]);
+
   const setImageClassName = (idx: number): string => {
     if (idx === imageIndex) {
       return 'gallery__list gallery__list--active';
     }
     return 'gallery__list gallery__list--hidden';
   };
+
   return (
     <div className='gallery'>
       <div className='gallery__thumbnails'>
@@ -31,7 +40,7 @@ function Gallery({ images }: Props) {
         </ul>
       </div>
       <div className='gallery__images'>
-        <div className='image__container'>
+        <div className='images__container'>
           <ul>
             {images.map((image, idx) => {
               return (
@@ -41,6 +50,25 @@ function Gallery({ images }: Props) {
                     className='product__image'
                     src={image.image}
                     alt='product'
+                    onMouseEnter={(e) => {
+                      // update image size and turn-on magnifier
+                      const elem = e.currentTarget;
+                      const { width, height } = elem.getBoundingClientRect();
+                      setSize([width, height]);
+                      setShowMagnifier(true);
+                    }}
+                    onMouseMove={(e) => {
+                      // update cursor position
+                      const elem = e.currentTarget;
+                      const { top, left } = elem.getBoundingClientRect();
+                      // calculate cursor position on the image
+                      const curX = e.pageX - left - window.pageXOffset;
+                      const curY = e.pageY - top - window.pageYOffset;
+                      setXY([curX, curY]);
+                    }}
+                    onMouseLeave={() => {
+                      setShowMagnifier(false);
+                    }}
                   />
                 </li>
               );
@@ -48,6 +76,35 @@ function Gallery({ images }: Props) {
           </ul>
         </div>
       </div>
+      <div
+        style={{
+          display: showMagnifier ? '' : 'none',
+          position: 'absolute',
+
+          // prevent magnifier blocks the mousemove event of img
+          pointerEvents: 'none',
+
+          // set size of magnifier
+          height: `${magnifierHeight}px`,
+          width: `${magnifierWidth}px`,
+          top: '0',
+          left: '100%',
+          opacity: '1',
+          border: '1px solid lightgray',
+          backgroundColor: 'white',
+          backgroundImage: `url('${images[imageIndex].image}')`,
+          backgroundRepeat: 'no-repeat',
+
+          // calculate zoomed image size
+          backgroundSize: `${imgWidth * zoomLevel}px ${
+            imgHeight * zoomLevel
+          }px`,
+
+          // calculate position of zoomed image.
+          backgroundPositionX: `${-x * zoomLevel + magnifierWidth / 2}px`,
+          backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`,
+        }}
+      />
     </div>
   );
 }
