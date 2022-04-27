e87e36e7f380923879fc10cbef5aedfb26862716
Aaron Lin <aaronlin74@gmail.com>
2022-04-27 23:07:23 +1000

    feat: create product schema and concate all schemas

--- amazonclone/schemas/schema.js
+++ amazonclone/schemas/schema.js
@@ -1,7 +1,18 @@
 import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
+import product from './product';
+import navbarLink from './navbarLink';
+import footerLink from './footerLink';
+import sidemenuLink from './sidemenuLink';
+import heroImage from './heroImage';
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([]),
+  types: schemaTypes.concat([
+    product,
+    navbarLink,
+    sidemenuLink,
+    footerLink,
+    heroImage,
+  ]),
 });
