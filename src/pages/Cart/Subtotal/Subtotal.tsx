a023c9d4f2b4223568ff4c34eb2133598eddfb1e
Aaron Lin <aaronlin74@gmail.com>
2022-05-02 22:54:32 +1000

    feat: add Stripe payment functionality
    1. only signed in user can perform the checkout. Unsigned in user will be redirect to sign in page.
    2. use Stripe client only integration approach to implement the payment function.
    3. disable the checkout button during the checkout process.

--- src/pages/Cart/Subtotal/Subtotal.tsx
+++ src/pages/Cart/Subtotal/Subtotal.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NumberFormat from 'react-number-format';
 import { AiFillCheckCircle } from 'react-icons/ai';
+import toast from 'react-hot-toast';
+import { RedirectToCheckoutOptions } from '@stripe/stripe-js';
+import { useNavigate } from 'react-router-dom';
+import getStripe from '../../../lib/stripePayment';
 import { getCartTotal, getCartItemNumber } from '../../../store/reducer';
 import { useStateValue } from '../../../components/StateProvider';
 import './Subtotal.scss';
 
 function Subtotal() {
-  const [{ cart }] = useStateValue();
+  const [{ cart, user }] = useStateValue();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [stripeError, setStripeError] = useState<string | null>(null);
   const subtotal = getCartTotal(cart);
   const numberOfItems = getCartItemNumber(cart);
+  const navigate = useNavigate();
 
+  const items = cart.map((item) => {
+    return { price: item.price_id, quantity: item.quantity };
+  });
+  const checkoutOptions: RedirectToCheckoutOptions = {
+    customerEmail: user ? user.email! : undefined,
+    lineItems: items,
+    mode: 'payment',
+    successUrl: `${window.location.origin}/success`,
+    cancelUrl: `${window.location.origin}/checkout`,
+    shippingAddressCollection: { allowedCountries: ['AU'] },
+    clientReferenceId: 'id',
+  };
+  const redirectToCheckout = async () => {
+    // Check if the client has signed in, if not redirect to sign in page
+    if (!user) {
+      navigate('/signin');
+      return;
+    }
+    setLoading(true);
+    try {
+      const stripe = await getStripe();
+      await stripe!.redirectToCheckout(checkoutOptions);
+    } catch (err) {
+      if (err instanceof Error) {
+        setStripeError(err.message);
+      } else {
+        setStripeError(String(err));
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Show the error notification
+  useEffect(() => {
+    toast.error(
+      stripeError || 'Oops! Something goes wrong, please try later again'
+    );
+  }, [stripeError]);
   return (
     <div className='subtotal'>
       <div className='subtotal__alert'>
@@ -43,9 +89,13 @@ function Subtotal() {
           <span className='gift__text'>This order contains a gift</span>
         </label>
       </div>
-
-      <button type='button' className='subtotal__btn'>
-        Proceed to Checkout
+      <button
+        type='button'
+        className='subtotal__btn'
+        onClick={redirectToCheckout}
+        disabled={loading}
+      >
+        {loading ? 'Loading...' : 'Proceed to Checkout'}
       </button>
     </div>
   );
