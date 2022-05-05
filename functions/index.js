9477d4b90de60d872bb62ab82b90481484787b2c
Aaron Lin <aaronlin74@gmail.com>
2022-05-06 09:59:22 +1000

    feat: add a webhook and verify order success route
    - add a webhook which listen to the Stripe event and call the fulfill order function.
    - add a order/success route so the client side could verify the checkout session and perform the success order operations like empty the cart.
    - change the success_url with addition route parameter of checkout session id for the client.
    - add region and timeout settings to the firebase functions.

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,13 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const express = require('express');
 const cors = require('cors');
 const admin = require('firebase-admin');
+
 admin.initializeApp();
 const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
+
+const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
 
 app.post('/create-checkout-session', async (req, res) => {
   try {
@@ -27,7 +30,7 @@ app.post('/create-checkout-session', async (req, res) => {
           quantity: item.quantity,
         };
       }),
-      success_url: `${req.headers.origin}/success`,
+      success_url: `${req.headers.origin}/success/{CHECKOUT_SESSION_ID}`,
       cancel_url: `${req.headers.origin}/checkout`,
       allow_promotion_codes: true,
     };
@@ -38,4 +41,58 @@ app.post('/create-checkout-session', async (req, res) => {
   }
 });
 
-exports.api = functions.https.onRequest(app);
+app.get('/order/success', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.retrieve(
+      req.query.session_id
+    );
+    const customer = await stripe.customers.retrieve(session.customer);
+    res.status(200).send({ customer });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+const fulfillOrder = (session) => {
+  // TODO: save order to db
+  console.log('Fulfilling order', session);
+};
+
+app.post(
+  '/webhook',
+  express.json({ type: 'application/json' }),
+  (request, response) => {
+    const payload = request.body;
+    let event;
+    if (endpointSecret) {
+      // Get the signature sent by Stripe
+      const signature = request.headers['stripe-signature'];
+      try {
+        event = stripe.webhooks.constructEvent(
+          payload,
+          signature,
+          endpointSecret
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return response.sendStatus(400);
+      }
+    }
+    // Handle the event
+    switch (event.type) {
+      case 'checkout.session.completed':
+        const session = event.data.object;
+        fulfillOrder(session);
+
+        break;
+      default:
+        // Unexpected event type
+        console.log(`Unhandled event type ${event.type}.`);
+    }
+    response.status(200).end();
+  }
+);
+
+exports.api = functions
+  .runWith({ timeoutSeconds: 200 })
+  .region('australia-southeast1')
+  .https.onRequest(app);
