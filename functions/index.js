f854f3befc88ca8cd52f7117ecc1232cc5a87991
Aaron Lin <aaronlin74@gmail.com>
2022-05-06 22:28:46 +1000

    feat: implement add successful order details to database
    - Since the stripe webhook needs raw request body, we exclude webhook route from using the JSON parser.
    - add metadata attribute for storing additional information about the purchuse.
    - implement fulfillOrder function, store the checkout info to the Firebase Firestore database

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,15 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 const app = express();
 app.use(cors({ origin: true }));
-app.use(express.json());
+
+// Use JSON parser for all non-webhook routes
+app.use((req, res, next) => {
+  if (req.path === '/webhook') {
+    next();
+  } else {
+    express.json()(req, res, next);
+  }
+});
 
 const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
 
@@ -24,6 +32,11 @@ app.post('/create-checkout-session', async (req, res) => {
         { shipping_rate: process.env.FREE_SHIPPING_ID },
         { shipping_rate: process.env.FAST_SHIPPING_ID },
       ],
+      metadata: {
+        cartItems: JSON.stringify(req.body.cartItems),
+        userId: req.body.userId,
+        createdAt: new Date().toString(),
+      },
       line_items: req.body.items,
       success_url: `${req.headers.origin}/success/{CHECKOUT_SESSION_ID}`,
       cancel_url: `${req.headers.origin}/checkout`,
@@ -47,45 +60,61 @@ app.get('/order/success', async (req, res) => {
     res.status(400).send(err);
   }
 });
-const fulfillOrder = (session) => {
-  // TODO: save order to db
-  console.log('Fulfilling order', session);
+
+const fulfillOrder = async (session) => {
+  // Write order info to the firestore
+  const userId = session.metadata.userId;
+  await admin
+    .firestore()
+    .collection(`users`)
+    .doc(userId)
+    .collection('orders')
+    .doc(session.id)
+    .set({
+      items: JSON.parse(session.metadata.cartItems),
+      amount: session.amount_total / 100,
+      createdAt: session.metadata.createdAt,
+    });
 };
 
-app.post(
-  '/webhook',
-  express.json({ type: 'application/json' }),
-  (request, response) => {
-    const payload = request.body;
-    let event;
-    if (endpointSecret) {
-      // Get the signature sent by Stripe
-      const signature = request.headers['stripe-signature'];
-      try {
-        event = stripe.webhooks.constructEvent(
-          payload,
-          signature,
-          endpointSecret
-        );
-      } catch (err) {
-        console.log(`⚠️  Webhook signature verification failed.`, err.message);
-        return response.sendStatus(400);
-      }
+// Stripe requires the raw body to construct the event
+app.post('/webhook', express.raw({ type: 'application/json' }), (req, res) => {
+  let event = req.body;
+  if (endpointSecret) {
+    // Get the signature sent by Stripe
+    const signature = req.headers['stripe-signature'];
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        signature,
+        endpointSecret
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`, err.message);
+      res.status(400).json({ message: err.message });
     }
-    // Handle the event
-    switch (event.type) {
-      case 'checkout.session.completed':
+  }
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.completed':
+      try {
         const session = event.data.object;
         fulfillOrder(session);
+      } catch (err) {
+        console.log(
+          'Something goes wrong when updating the firestore database',
+          err.message
+        );
+        res.status(400).json({ message: err.message });
+      }
 
-        break;
-      default:
-        // Unexpected event type
-        console.log(`Unhandled event type ${event.type}.`);
-    }
-    response.status(200).end();
+      break;
+    default:
+      // Unexpected event type
+      console.log(`Unhandled event type ${event.type}.`);
   }
-);
+  res.status(200).end();
+});
 
 exports.api = functions
   .runWith({ timeoutSeconds: 200 })
